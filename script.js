6db2334cb0e5fc0548c9e48fd8a0a131c8e69135
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-02-28 14:44:07 +0300

    Solving problems in JS

---
+++ script.js
@@ -0,0 +1,31 @@
+// Task 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Task 2
+let years = 2007;
+alert(years);
+
+// Task 3
+let creatorJS = "Брендан Эйх";
+alert(creatorJS);
+
+// Task 4
+let num1 = 10;
+let num2 = 2;
+let summ, differnce, multiplication, division;
+summ = num1 + num2;
+differnce = num1 - num2;
+multiplication = num1 * num2;
+division = num1 / num2;
+alert(`Сумма ${num1} и ${num2} равна ${summ}, 
+Разность ${num1} и ${num2} равна ${differnce},
+Произведение ${num1} и ${num2} равна ${multiplication},
+Деление ${num1} и ${num2} равна ${division}`);
+
+// Task 5
+let num = 2;
+let result = num ** 5;
+alert(result)
