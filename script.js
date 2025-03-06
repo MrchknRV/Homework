92161f8ed78629662c962574c9ab54c99e839ae9
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-03-06 14:21:05 +0300

    Task 5 is completed

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ if (password !== user_password.toLocaleLowerCase()){
 } else {
     alert("Пароль введен верно")
 }
+
 // Task 2
 let c = 2;
 if (c > 0 && c < 10) {
@@ -13,6 +14,7 @@ if (c > 0 && c < 10) {
 } else {
     console.log("Неверно");
 }
+
 // Task 3
 let d = 20;
 let e = 1;
@@ -21,8 +23,54 @@ if (d > 100 || e > 100) {
 } else {
     console.log("Неверно");
 }
+
 // Task 4
 let a = '2';
 let b = '3';
 // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
 alert(Number(a) + Number(b));
+
+// Task 5
+let month_number = 12
+switch (month_number) {
+    case 1:
+        console.log('зима');
+        break
+    case 2:
+        console.log('зима');
+        break
+    case 3:
+        console.log('весна');
+        break
+    case 4:
+        console.log('весна');
+        break
+    case 5:
+        console.log('весна');
+        break
+    case 6:
+        console.log('лето');
+        break
+    case 7:
+        console.log('лето');
+        break
+    case 8:
+        console.log('лето');
+        break
+    case 9:
+        console.log('осень');
+        break
+    case 10:
+        console.log('осень');
+        break
+    case 11:
+        console.log('осень');
+        break
+    case 12:
+        console.log('зима');
+        break
+    
+    default:
+        console.log("Нет такого");
+            
+}
