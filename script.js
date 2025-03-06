8ffda60e8a0f1dd5467bc73427e2725e86215140
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-03-06 14:08:22 +0300

    Solving homework problems

--- script.js
+++ script.js
@@ -1,62 +1,23 @@
-// // Task 1
-// let a = 10;
-// alert(a);
-// a = 20;
-// alert(a);
-
-// // Task 2
-// let years = 2007;
-// alert(years);
-
-// // Task 3
-// let creatorJS = "Брендан Эйх";
-// alert(creatorJS);
-
-// // Task 4
-// let num1 = 10;
-// let num2 = 2;
-// let summ, differnce, multiplication, division;
-// summ = num1 + num2;
-// differnce = num1 - num2;
-// multiplication = num1 * num2;
-// division = num1 / num2;
-// alert(`Сумма ${num1} и ${num2} равна ${summ}, 
-// Разность ${num1} и ${num2} равна ${differnce},
-// Произведение ${num1} и ${num2} равна ${multiplication},
-// Деление ${num1} и ${num2} равна ${division}`);
-
-// // Task 5
-// let num = 2;
-// let result = num ** 5;
-// alert(result)
-
-// // Task 6
-// let aa = 9;
-// let bb = 2;
-// alert(aa % bb);
-
-// // Task 7
-// let numb = 1;
-// numb += 5;
-// numb -= 3;
-// numb *= 7;
-// numb /= 3;
-// numb++;
-// numb--;
-// alert(num);
-
-// // Task 8
-// let age = prompt("Сколько вам лет?")
-// alert(age)
-
-// // Task 9
-// const user = {
-//     nname: 'Rodion',
-//     aage: 23,
-//     isAdmin: false
-// }
-// alert(`${user.nname} - ${user.aage}`)
-
-// // Task 10
-// let NAME = prompt("What's your name?")
-// alert(`Hello, ${NAME}`)
+// Task 1
+let password = 'пароль'
+let user_password = String(prompt("Введите пароль:"))
+if (password !== user_password.toLocaleLowerCase()){
+    alert("Пароль введен неправильно")
+} else {
+    alert("Пароль введен верно")
+}
+// Task 2
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
+// Task 3
+let d = 20;
+let e = 1;
+if (d > 100 || e > 100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
