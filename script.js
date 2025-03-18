0ce7958e45bf93886f1c6a8e311017c933b9f841
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-03-18 09:56:19 +0300

    Task 2 is completed

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// Task 1
-let i = 0;
-while (i < 2){
-    console.log('Привет');
+// // Task 1
+// let i = 0;
+// while (i < 2){
+//     console.log('Привет');
+//     i++
+// }
+// Task 2
+let i = 1;
+while (i <= 5){
+    console.log(i);
     i++
 }
