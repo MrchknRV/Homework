ef429f2d7f77a3451e5f7d85a341a9dcf6cc2f09
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-03-18 10:22:26 +0300

    Task 6 is completed

--- script.js
+++ script.js
@@ -25,11 +25,16 @@
 // for (let key in obj){
 //     console.log(`${key} - зарплата ${obj[key]} долларов`);
 // }
-// Task 5
-let num = 0;
-let n = 1000;
-while (n > 50){
-    num++
-    n /= 2
+// // Task 5
+// let num = 0;
+// let n = 1000;
+// while (n > 50){
+//     num++
+//     n /= 2
+// }
+// console.log(`Кол-во итерацией ${num}, полученное число ${n}`);
+// Task 6
+const friday = 2;
+for (let day = friday; day <= 31; day += 7 ){
+    console.log(`Сегодня пятница ${day}-e число. Необходимо подготовить еженедельный отчет`);
 }
-console.log(`Кол-во итерацией ${num}, полученное число ${n}`);
